b10a30adbc5a5414d5c3bbe1b8da982416313870
Bruno Zell <contact@brunozell.com>
2023-04-01 13:08:12 +0200

    update runtime data model for persistence

--- source/AskFi.Runtime.DataModel/Runtime.DataModel.fs
+++ source/AskFi.Runtime.DataModel/Runtime.DataModel.fs
@@ -4,17 +4,17 @@ open AskFi
 open System
 open AskFi.Sdk
 
-// ############################
-// #### OBSERVER SUBSYSTEM ####
-// ############################
+// ###############################
+// #### OBSERVATION SUBSYSTEM ####
+// ###############################
 
-/// Raw data structure produced by an Observer instance.
-/// All new information received via this Observer instance is referenced in this tree.
+/// Raw data structure produced by an IObserver-instance.
+/// All new information received via this IObserver-instance is referenced in this tree.
 type ObservationSequenceHead<'Perception> =
     | Beginning
     | Observation of ObservationSequenceNode<'Perception>
 and ObservationSequenceNode<'Perception> = {
-    /// All observations that happened at this instant.
+    /// All perceptions that appeared at this instant.
     Observation: Sdk.Observation<'Perception>
 
     /// Link to the previous observations of this session, forming a linked list and sequencing them.
@@ -24,15 +24,22 @@ and ObservationSequenceNode<'Perception> = {
 
 // Todo: Add index trees: ChronologicalObservationSequence, ContinuousObservationSequence, ChronologicalContinuousObservationSequence
 
-// ###########################
-// #### SESSION SUBSYSTEM ####
-// ###########################
-
-type Timestamp = DateTime
-
 /// Updates to multiple Observation Sequences are sequenced with each other into a Perspective Sequence.
 /// This defines an ordering between observations from different Observation Sequences (and implicitly, different IObserver-instances)
 /// and merges them into a single sequence of observations (across all Perception-types).
 type PerspectiveSequenceHead =
     | Empty
-    | Happening of at:Timestamp (*as of runtime clock*) * previous:ContentId * observationStreamHead:ContentId // actually ObservationSequenceHead<_> of all possible types. Todo: implement as recursion scheme
+    | Happening of PerspectiveSequenceNode
+and PerspectiveSequenceNode = {
+    /// Absolute timestamp of when this happening occurred.
+    /// As of runtime clock.
+    At: DateTime
+
+    /// Links previous PerspectiveSequenceHead. This sequencing creates a temporal order between IObserver-instances.
+    Previous: ContentId // PerspectiveSequenceHead
+
+    /// Link to the updated ObservationSequenceHead<_> that caused this update in perspective.
+    /// ObservationSequenceHead<_> of all possible types.
+    /// Todo: implement as recursion scheme
+    ObservationStreamHead: ContentId // ObservationSequenceHead<_>
+}
