49be3c7dd0278377b17515f78fb21f12b45edb48
Bruno Zell <contact@brunozell.com>
2023-04-01 15:27:19 +0200

    use newtonsoft to deserialize F# discriminated unions

--- source/AskFi.Runtime/Persistence/XxHashJsonSerializer.cs
+++ source/AskFi.Runtime/Persistence/XxHashJsonSerializer.cs
@@ -1,6 +1,6 @@
 using System.Text;
-using System.Text.Json;
 using AskFi.Persistence;
+using Newtonsoft.Json;
 using Standart.Hash.xxHash;
 
 namespace AskFi.Runtime.Persistence;
@@ -9,7 +9,7 @@ internal class XxHashJsonSerializer : Serializer
 {
     public EncodedIdea serialize<TIdea>(TIdea value)
     {
-        var json = JsonSerializer.Serialize(value);
+        var json = JsonConvert.SerializeObject(value);
         var bytes = Encoding.UTF8.GetBytes(json);
         var hash = xxHash128.ComputeHash(bytes, bytes.Length).ToBytes();
 
