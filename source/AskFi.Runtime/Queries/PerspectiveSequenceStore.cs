bb3c30558222ad390985fab41a76eb411be1bdd0
Bruno Zell <contact@brunozell.com>
2023-04-01 12:57:37 +0200

    use idea store to store perspectives

--- source/AskFi.Runtime/Queries/PerspectiveSequenceStore.cs
+++ source/AskFi.Runtime/Queries/PerspectiveSequenceStore.cs
@@ -1,34 +1,36 @@
-using System.Collections.Concurrent;
+using AskFi.Runtime.Persistence;
 using static AskFi.Runtime.DataModel;
 
 namespace AskFi.Runtime.Queries;
 
-internal static class PerspectiveSequenceStore
+internal class PerspectiveSequenceStore
 {
-    private static readonly ConcurrentDictionary<int, PerspectiveSequenceHead> History = new();
+    private readonly IdeaStore _store;
+
+    public PerspectiveSequenceStore(IdeaStore store)
+    {
+        _store = store;
+    }
 
     /// <inheritdoc cref="LookupSequencePosition(Sdk.Perspective)"/>
-    internal static PerspectiveSequenceHead LookupSequencePosition(int perspectiveSequenceHeadHash)
+    public ValueTask<PerspectiveSequenceHead> LookupSequencePosition(Sdk.ContentId perspectiveSequenceHeadCid)
     {
-        return History[perspectiveSequenceHeadHash];
+        return _store.Load<PerspectiveSequenceHead>(perspectiveSequenceHeadCid);
     }
 
     /// <summary>
     /// Looks up the underlying <see cref="PerspectiveSequenceHead"/> behind the passed <paramref name="perspective"/>.
     /// Throws if the <see cref="PerspectiveSequenceHead"/> has not been inserted via <see cref="Store(PerspectiveSequenceHead)"/> first.
     /// </summary>
-    internal static PerspectiveSequenceHead LookupSequencePosition(Sdk.Perspective perspective) =>
-        LookupSequencePosition(perspective.HashOfLatestPerspectiveSequenceHead);
+    public ValueTask<PerspectiveSequenceHead> LookupSequencePosition(Sdk.Perspective perspective) =>
+        LookupSequencePosition(perspective.LatestPerspectiveSequenceHead);
 
     /// <summary>
     /// Called by <see cref="Behavior.PerspectiveSequencer"/> on every new observation.
     /// </summary>
     /// <param name="perspectiveSequence">The latest <see cref="PerspectiveSequenceHead"/> with the new observation as its head.</param>
-    internal static PerspectiveHash Store(PerspectiveSequenceHead perspectiveSequence)
+    public ValueTask<Sdk.ContentId> Store(PerspectiveSequenceHead perspectiveSequence)
     {
-        var rawHash = perspectiveSequence.GetHashCode();
-        History.TryAdd(rawHash, perspectiveSequence);
-        var hash = PerspectiveHash.NewHash(rawHash);
-        return hash;
+        return _store.Store(perspectiveSequence);
     }
 }
