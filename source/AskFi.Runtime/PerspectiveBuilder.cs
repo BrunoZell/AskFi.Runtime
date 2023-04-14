4e52b4356c0868916f4945a3ae18998ee1c2b756
Bruno Zell <contact@brunozell.com>
2023-04-14 02:35:06 +0200

    clean perspective builder

---
+++ source/AskFi.Runtime/PerspectiveBuilder.cs
@@ -0,0 +1,25 @@
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using AskFi.Persistence;
+
+namespace AskFi.Runtime;
+
+public class PerspectiveBuilder
+{
+    private readonly ConcurrentDictionary<Type, ImmutableHashSet<ContentId>> _observations = new();
+
+    public void WithObservation<TPerception>(ContentId observationCid)
+    {
+        _observations.AddOrUpdate(
+            key: typeof(TPerception),
+            addValueFactory: (key, cid) => ImmutableHashSet.Create(cid),
+            updateValueFactory: (key, existing, cid) => existing.Add(cid),
+            factoryArgument: observationCid);
+    }
+
+    public Sdk.Perspective Build()
+    {
+        // Todo: Merge all observations into a Perspective
+        return new Sdk.Perspective();
+    }
+}
