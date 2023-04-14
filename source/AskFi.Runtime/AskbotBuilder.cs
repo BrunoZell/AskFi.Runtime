946153892ced3c64562262a5ba000c5bc13f3b80
Bruno Zell <contact@brunozell.com>
2023-04-14 05:30:03 +0200

    fix askbot builder

--- source/AskFi.Runtime/AskbotBuilder.cs
+++ source/AskFi.Runtime/AskbotBuilder.cs
@@ -1,25 +1,26 @@
 using AskFi.Runtime.Persistence;
+using AskFi.Runtime.Queries;
 using StrategyDelegate = System.Func<AskFi.Sdk.Reflection, AskFi.Sdk.Perspective, AskFi.Sdk.Decision>;
 
 namespace AskFi.Runtime;
 
 public class AskbotBuilder
 {
-    private readonly Dictionary<Type, object> _observers = new(); // of type IObserver<Perception> (where Perception = .Key)
+    private IPerspectiveSource? _perspectiveSource;
     private readonly Dictionary<Type, object> _brokers = new(); // of type IBroker<Action> (where Action = .Key)
     private StrategyDelegate? _strategy;
     private DirectoryInfo? _localPersistenceDirectory;
     private Uri? _ipfsClusterUrl;
 
-    public void AddObserver<TPerception>(Sdk.IObserver<TPerception> observer)
+    public void WithPerspective(IPerspectiveSource perspectiveSource)
     {
-        var added = _observers.TryAdd(typeof(TPerception), observer);
-
-        if (!added) {
+        if (_perspectiveSource is not null) {
             throw new InvalidOperationException(
-                $"An observer for perception type '{typeof(TPerception).FullName}' has already been added to " +
-                $"this Askbot Builder. Only one observer instance per perception-type can be used.");
+                $"A Perspective Source already has been added to this Askbot Builder." +
+                $"Only one Perspective Source per Askbot Instance can be used.");
         }
+
+        _perspectiveSource = perspectiveSource;
     }
 
     public void AddBroker<TAction>(Sdk.IBroker<TAction> broker)
@@ -82,6 +83,6 @@ public class AskbotBuilder
         _localPersistenceDirectory.Create();
         var storageEnvironment = new StorageEnvironment(_localPersistenceDirectory, _ipfsClusterUrl);
 
-        return new Askbot(_observers, _brokers, _strategy, storageEnvironment);
+        return new Askbot(_perspectiveSource, _brokers, _strategy, storageEnvironment);
     }
 }
