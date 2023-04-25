9403cd71a8f73be9f4a50a77c0ab0497f4037c58
Bruno Zell <contact@brunozell.com>
2023-04-25 14:38:55 +0200

    move platform interfaces into data model project

--- source/AskFi.Runtime/Platform/IPlatformMessaging.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-namespace AskFi.Runtime.Platform;
-
-public interface IPlatformMessaging
-{
-    void Emit<TMessage>(TMessage message);
-    IAsyncEnumerable<TMessage> Listen<TMessage>([EnumeratorCancellation] CancellationToken cancellationToken);
-}
--- source/AskFi.Runtime/Platform/IPlatformState.cs
+++
@@ -1,7 +0,0 @@
-namespace AskFi.Runtime.Platform;
-
-public interface IPlatformState
-{
-    public Task Write(ReadOnlySpan<byte> key, ReadOnlySpan<byte> value);
-    public Task<ReadOnlyMemory<byte>> Read(ReadOnlySpan<byte> key);
-}
