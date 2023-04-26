502be53d9bd724c9fca8d935b50b479c3866d6df
Bruno Zell <contact@brunozell.com>
2023-04-26 15:55:17 +0200

    cleanup perspective module

--- source/AskFi.Runtime/Modules/Perspective/PerspectiveModule.cs
+++ source/AskFi.Runtime/Modules/Perspective/PerspectiveModule.cs
@@ -1,15 +1,34 @@
+using System.Threading.Channels;
 using AskFi.Runtime.Messages;
 using AskFi.Runtime.Platform;
 
 namespace AskFi.Runtime.Modules.Perspective;
-internal class PerspectiveModule
+internal class PerspectiveModule : IPerspectiveModule
 {
-    public async Task Run(IPlatformMessaging messaging, CancellationToken cancellationToken)
+    private readonly Channel<NewObservation> _input;
+    private readonly Channel<NewPerspective> _output;
+    private readonly IPlatformPersistence _persistence;
+
+    ChannelWriter<NewObservation> IPerspectiveModule.Input => _input.Writer;
+    ChannelReader<NewPerspective> IPerspectiveModule.Output => _output.Reader;
+
+    public PerspectiveModule(IPlatformPersistence persistence)
     {
-        await foreach (var newObservation in messaging.Listen<NewObservation>(cancellationToken)) {
-            messaging.Emit<NewPerspective>(new() {
-                PerspectiveSequenceHeadCid = default
-            });
+        _input = Channel.CreateUnbounded<NewObservation>();
+        _output = Channel.CreateUnbounded<NewPerspective>();
+        _persistence = persistence;
+    }
+
+    public async Task Run(CancellationToken cancellationToken)
+    {
+        await foreach (var newObservation in _input.Reader.ReadAllAsync(cancellationToken)) {
+            // Todo: Merge perspectives
+            // Todo: Publish state
+            var newPerspective = new NewPerspective(
+                perspectiveSequenceNodeCid: default,
+                rewriteDepth: 0);
+
+            await _output.Writer.WriteAsync(newPerspective);
         }
     }
 }
