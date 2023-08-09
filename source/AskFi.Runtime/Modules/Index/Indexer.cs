ed4c423d7ec5bcf0a1e462f934200318b6a8c35f
Bruno Zell <contact@brunozell.com>
2023-08-09 22:40:53 +0200

    [WIP] add sequencer and indexer

---
+++ source/AskFi.Runtime/Modules/Index/Indexer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AskFi.Runtime.DataModel;
+using static AskFi.Runtime.Index;
+
+namespace AskFi.Runtime.Modules.Index;
+
+internal static class Indexer
+{
+    public static ContextSequenceHead Advance(ContextSequenceIndex ContextSequenceHead previousContext, ObservationSequenceHead newObservation)
+    {
+
+    }
+}
