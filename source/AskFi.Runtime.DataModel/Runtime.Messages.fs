d37253f7a75fc95f010d41cf42122e7e8d6500bc
Bruno Zell <contact@brunozell.com>
2023-08-16 14:53:49 +0200

    new context message

--- source/AskFi.Runtime.DataModel/Runtime.Messages.fs
+++ source/AskFi.Runtime.DataModel/Runtime.Messages.fs
@@ -2,21 +2,30 @@ namespace AskFi.Runtime.Messages
 
 open AskFi.Runtime.Persistence
 
-/// Message from Observer Module emitted on each new received observation,
-/// hopefully appended to its previous version of the single observation sequence it produces.
+/// Message from Observer Group emitted on each new received observation,
+/// in parallel with updating that observation sequence in the platform state.
+/// If valid, it's appended to the previous version of the single observation sequence an Observer Group produces.
 type NewObservation = {
     /// Cid to the original ObservationSequenceHead.Identity of the newly produced observation sequence head.
     Identity: ContentId
 
     /// Cid to the newly produced ObservationSequenceHead.Observation
+    /// To be a valid message, this.Head must carry the identity of this.Identity
     Head: ContentId
 }
 
-/// Nodes send this message to gossip the latest knowledge base.
-/// Knowledge base gossiper send them out when they received a NewObservation message.
-type NewKnowledgeBase = {
-    /// Cid to the latest heaviest knowledge base
-    KnowledeBase: ContentId // KnowledeBase
+/// Message from Sequencer whenever 
+type NewContext = {
+    /// Cid to the original ContextSequenceHead.Identity of the newly produced context sequence head.
+    Identity: ContentId
+
+    /// Cid to the newly produced and currently latest ContextSequenceHead.Observation
+    /// To be a valid message, this.Head must carry the identity of this.Identity
+    Head: ContentId
+
+    /// Cid to the full history, including rewinds, the sequencer has produced.
+    /// To be a valid message, this.Head must be the latest context sequence head from thus.History
+    History: ContentId
 }
 
 /// Message emitted from Strategy Module when a non-inaction decision has been made
